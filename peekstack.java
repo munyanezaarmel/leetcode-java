54af759df353d974aaca297faa5f83aa816a6843
Munyaneza Armel <munyaarmel61@gmail.com>
2023-01-03 19:15:38 +0200

    stack operations

---
+++ peekstack.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) {
+        Stack<String> animals= new Stack<>();
+
+        // Add elements to Stack
+        animals.push("Dog");
+        animals.push("Horse");
+        animals.push("Cat");
+        System.out.println("Stack: " + animals);
+
+        // Access element from the top
+        String element = animals.peek();
+        System.out.println("Element at top: " + element);
+
+    }
+}
