5278acd6adc9552eab468429ba7c307a90187133
Munyaneza Armel <munyaarmel61@gmail.com>
2023-01-12 05:50:31 +0200

    quick find

---
+++ quickfind.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Arrays;
+class QuickFind {
+    private int[] integerArray;
+    public int[] getIntegerArray{
+        return Array
+    }
+    public void setIntegerArray(int[]integerArray){
+        this.integerArray=integerArray;
+    }
+    public quickFind(int length){
+        this.integerArray=new int [length];
+        for(int i=0;i<integerArray.length;i++){
+            this.integerArray[i]=new Random().nextInt(20)+1;
+        }
+    }
+    public static void main(String[] args) {
+      
+    }
+}
